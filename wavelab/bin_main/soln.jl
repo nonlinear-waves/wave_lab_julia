60d270ff8b7c874619c519ba872f38eab7f78e05
jakelowellmurphy <murphy.jake144@gmail.com>
2024-06-30 01:19:19 -0600

    Implemented soln

--- wavelab/bin_main/soln.jl
+++ wavelab/bin_main/soln.jl
@@ -4,6 +4,15 @@ function soln(x,s)
     # Input "x" is the value where the solution is evaluated and "s" is a
     # stucture described in the STABLAB documentation
 
+    if x < 0
+        x = s.side * s.I * (x / s.L)
+        temp = s.sol(x)
+        out = temp[s.larray, :]
+    else
+        x = s.side * s.I * (x/s.R)
+        temp = s.sol(x)
+        out = temp[s.rarray, :]
+    end
 
-    return nothing
+    return out
 end
